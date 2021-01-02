a73d8b257c559f3983a25d4a402344edd4f5f22b
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-02 13:28:58 -0500

    Finished LoadFromXml method for NonDerivativeTransaction

--- src/NonDerivativeTransaction.cs
+++ src/NonDerivativeTransaction.cs
@@ -36,10 +36,55 @@ namespace SecuritiesExchangeCommission.Edgar
                 if (node_transactionCode != null)
                 {
                     string tc = node_transactionCode.InnerText.ToLower();
-                    
+                    TransactionCode = TransactionTypeFromCode(tc);
                 }
             }
 
+            //Transaction Amounts section (# of shares, price per share, acquired or disposed)
+            XmlNode node_transactionAmounts = node.SelectSingleNode("transactionAmounts");
+            if (node_transactionAmounts != null)
+            {
+                //Transaction Shares Quanity
+                XmlNode node_transactionShares = node_transactionAmounts.SelectSingleNode("transactionShares");
+                if (node_transactionShares != null)
+                {
+                    XmlNode node_value = node_transactionShares.SelectSingleNode("value");
+                    if (node_value != null)
+                    {
+                        TransactionShares = Convert.ToUInt32(node_value.InnerText);
+                    }
+                }
+
+                //Transaction price per share
+                XmlNode node_transactionPricePerShare = node_transactionAmounts.SelectSingleNode("transactionPricePerShare");
+                if (node_transactionPricePerShare != null)
+                {
+                    XmlNode node_value = node_transactionPricePerShare.SelectSingleNode("value");
+                    if (node_value != null)
+                    {
+                        PricePerShare = Convert.ToSingle(node_value.InnerText);
+                    }
+                }
+
+                //Acquired or disposed?
+                XmlNode node_transactionAcquiredDisposedCode = node_transactionAmounts.SelectSingleNode("transactionAcquiredDisposedCode");
+                if (node_transactionAcquiredDisposedCode != null)
+                {
+                    XmlNode node_value = node_transactionAcquiredDisposedCode.SelectSingleNode("value");
+                    if (node_value != null)
+                    {
+                        string ad_code = node_value.InnerText.ToLower();
+                        if (ad_code == "a")
+                        {
+                            AcquiredOrDisposed = AcquiredDisposed.Acquired;
+                        }
+                        else if (ad_code == "d")
+                        {
+                            AcquiredOrDisposed = AcquiredDisposed.Disposed;
+                        }
+                    }
+                }
+            }
         }
 
         private TransactionType TransactionTypeFromCode(string code)
