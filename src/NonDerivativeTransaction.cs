89775c28039d5a9f50b0709c203a0b0ca9812220
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-02 20:35:26 -0500

    Made the transaction code to enum method static

--- src/NonDerivativeTransaction.cs
+++ src/NonDerivativeTransaction.cs
@@ -36,7 +36,7 @@ namespace SecuritiesExchangeCommission.Edgar
                 if (node_transactionCode != null)
                 {
                     string tc = node_transactionCode.InnerText.ToLower();
-                    TransactionCode = TransactionTypeFromCode(tc);
+                    TransactionCode = StatementOfChangesInBeneficialOwnership.TransactionTypeFromCode(tc);
                 }
             }
 
@@ -87,40 +87,6 @@ namespace SecuritiesExchangeCommission.Edgar
             }
         }
 
-        private TransactionType TransactionTypeFromCode(string code)
-        {
-            List<KeyValuePair<string, TransactionType>> KVPs = new List<KeyValuePair<string, TransactionType>>();
-            KVPs.Add(new KeyValuePair<string, TransactionType>("P", TransactionType.OpenMarketOrPrivatePurchase));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("S", TransactionType.OpenMarketOrPrivateSale));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("V", TransactionType.TransactionVoluntarilyReportedEarlierThanRequired));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("A", TransactionType.GrantOrAward));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("D", TransactionType.SaleBackToIssuer));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("F", TransactionType.PaymentOfExcersizePriceOrTaxLiability));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("I", TransactionType.DiscretionaryTransaction));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("M", TransactionType.ExcersizeOrConversionOfDerivativeSecurity));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("C", TransactionType.ConversionOfDerivativeSecurity));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("E", TransactionType.ExpirationOfShortDerivativePosition));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("H", TransactionType.ExpirationOfLongDerivativePosition));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("O", TransactionType.ExcersizeOfOutOfMoneyDerivative));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("X", TransactionType.ExcersizeOfInMoneyDerivative));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("G", TransactionType.BonaFideGift));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("L", TransactionType.SmallAcquisition));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("W", TransactionType.AcquisitionOrDispositionByWillOrLaws));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("Z", TransactionType.DepositIntoOrWithdrawalFromVotingTrust));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("J", TransactionType.OtherAcquisitionOrDisposition));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("K", TransactionType.TransactionInEquitySwap));
-            KVPs.Add(new KeyValuePair<string, TransactionType>("U", TransactionType.DispositionDueToTenderOfShares));
         
-            foreach (KeyValuePair<string, TransactionType> kvp in KVPs)
-            {
-                if (kvp.Key == code.ToUpper())
-                {
-                    return kvp.Value;
-                }
-            }
-
-            throw new Exception("Unable to find transaction type for code '" + code + "'");
-        }
-
     }
 }
