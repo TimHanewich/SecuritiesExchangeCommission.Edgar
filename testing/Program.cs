153225931cdd5a99269b4f255612746100a66581
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-02 19:47:49 -0500

    Additional testing

--- testing/Program.cs
+++ testing/Program.cs
@@ -13,30 +13,30 @@ namespace testing
     {
         static void Main(string[] args)
         {
-            string content = System.IO.File.ReadAllText("C:\\Users\\TaHan\\Downloads\\msft_form4_fail.xml");
-            StatementOfChangesInBeneficialOwnership form4 = StatementOfChangesInBeneficialOwnership.ParseXml(content);
-            Console.WriteLine(JsonConvert.SerializeObject(form4));
+            // string content = System.IO.File.ReadAllText("C:\\Users\\tihanewi\\Downloads\\Form4\\bac.xml");
+            // StatementOfChangesInBeneficialOwnership form4 = StatementOfChangesInBeneficialOwnership.ParseXml(content);
+            // Console.WriteLine(JsonConvert.SerializeObject(form4));
 
 
-            // HttpClient hc = new HttpClient();
-            // EdgarSearch es = EdgarSearch.CreateAsync("intc", "4", null, EdgarSearchOwnershipFilter.include).Result;
-            // foreach (EdgarSearchResult esr in es.Results)
-            // {
-            //     if (esr.Filing == "4")
-            //     {
-            //         FilingDocument[] docs = esr.GetDocumentFormatFilesAsync().Result;
-            //         foreach (FilingDocument fd in docs)
-            //         {
-            //             if (fd.DocumentName.ToLower().Contains(".xml"))
-            //             {
-            //                 Console.WriteLine("Tryng to get " + fd.Url + " ...");
-            //                 HttpResponseMessage hrm = hc.GetAsync(fd.Url).Result;
-            //                 string content = hrm.Content.ReadAsStringAsync().Result;
-            //                 StatementOfChangesInBeneficialOwnership form4 = StatementOfChangesInBeneficialOwnership.ParseXml(content);
-            //             }
-            //         }
-            //     }
-            // }
+            HttpClient hc = new HttpClient();
+            EdgarSearch es = EdgarSearch.CreateAsync("snap", "4", null, EdgarSearchOwnershipFilter.only).Result;
+            foreach (EdgarSearchResult esr in es.Results)
+            {
+                if (esr.Filing == "4")
+                {
+                    FilingDocument[] docs = esr.GetDocumentFormatFilesAsync().Result;
+                    foreach (FilingDocument fd in docs)
+                    {
+                        if (fd.DocumentName.ToLower().Contains(".xml"))
+                        {
+                            Console.WriteLine("Tryng to get " + fd.Url + " ...");
+                            HttpResponseMessage hrm = hc.GetAsync(fd.Url).Result;
+                            string content = hrm.Content.ReadAsStringAsync().Result;
+                            StatementOfChangesInBeneficialOwnership form4 = StatementOfChangesInBeneficialOwnership.ParseXml(content);
+                        }
+                    }
+                }
+            }
 
         }
     }
