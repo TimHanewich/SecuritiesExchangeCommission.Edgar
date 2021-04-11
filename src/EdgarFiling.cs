6eea3df18ecea596596b8900366809b0c5e0a559
Tim Hanewich <tahanewich@live.com>
2021-04-11 17:16:35 -0400

    EdgarFiling now uses the standard prep request message

--- src/EdgarFiling.cs
+++ src/EdgarFiling.cs
@@ -51,9 +51,8 @@ namespace SecuritiesExchangeCommission.Edgar
 
             //Get the content
             HttpClient hc = new HttpClient();
-            HttpRequestMessage req = new HttpRequestMessage();
+            HttpRequestMessage req = SecToolkit.PrepareHttpRequestMessage();
             req.RequestUri = new Uri(DocumentsUrl);
-            req.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.75"); //This identifies the request as coming from a browser. If we do not provide info, the SEC will flag this as coming from an undeclared tool.
             HttpResponseMessage hrm = await hc.SendAsync(req);
             string web = await hrm.Content.ReadAsStringAsync();
 
