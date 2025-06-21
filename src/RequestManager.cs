3dbe949b507906fadc401ddfeff0234943c831a3
Tim Hanewich <tahanewich@live.com>
2025-06-21 15:51:39 -0400

    Added new RequestManager

---
+++ src/RequestManager.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace SecuritiesExchangeCommission.Edgar.Data
+{
+    public class RequestManager
+    {
+        //Modifiable propertie
+        public string AppName { get; set; }
+        public string AppVersion { get; set; }
+        public string Email { get; set; }
+
+        //Create version of it
+        private static readonly RequestManager _instance = new RequestManager();
+
+        //Reference
+        public static RequestManager Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public string ToUserAgent()
+        {
+            return AppName + "/" + AppVersion + " (" + Email + ")";
+        }
+        
+    }
+}
